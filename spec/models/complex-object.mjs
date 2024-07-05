95a2cb8ba902d3be7d7c92da8b05afb6b3cf1c70
marchuanv <marchuanv@gmail.com>
2024-07-05 20:12:42 +0200

    refactor

--- spec/models/complex-object.mjs
+++ spec/models/complex-object.mjs
@@ -1,7 +1,7 @@
 import { SchemaModel } from "../../lib/schema-model.mjs";
 export class ComplexObject extends SchemaModel {
     get name() { }
-    // get age() { }
+    set age(val) { }
     get address() { }
     get hobbies() { }
 }
