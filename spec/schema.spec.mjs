25dd807b40ca1f0fd32def18adae7ed2ebbb9ec8
marchuanv <marchuanv@gmail.com>
2024-07-07 01:43:02 +0200

    cleanup and adding tests

--- spec/schema.spec.mjs
+++ spec/schema.spec.mjs
@@ -1,9 +1,10 @@
-import { getModels } from 'schema.jsmodel';
+import { getModels, SchemaLoader } from 'schema.jsmodel';
 describe(('Schemas'), () => {
     describe(('given a schema'), () => {
         it('should', async () => {
-            const models = await getModels('./spec/schemas/complex.schema.json');
-            expect(models).toBeDefined();
+            const schemaLoader = new SchemaLoader('./spec/schemas/complex.schema.json');
+            const models = await getModels(schemaLoader);
+            expect(models.length).toBe(2);
         });
     });
 });
