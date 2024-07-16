7dce0da0f82fad9944de43a4a7e3e615dcf49d05
marchuanv <marchuanv@gmail.com>
2024-07-16 23:28:44 +0200

    adding more tests

--- spec/schema.spec.mjs
+++ spec/schema.spec.mjs
@@ -5,6 +5,8 @@ describe(('Schemas'), () => {
             const schemaLoader = new SchemaLoader('./spec/schemas/complex.schema.json');
             const models = await getModels(schemaLoader);
             const { instance } = models.find(x => x.type.name === 'AddressModel');
+            instance.postalCode = 1232134;
+            instance.Id = 'awdawdawd';
             const id = instance.Id;
             const postalCode = instance.postalCode;
             expect(models.length).toBe(2);
