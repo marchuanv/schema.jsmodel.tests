5b0cd472164fb2737fdcfc619479ecdb775f908f
marchuanv <marchuanv@gmail.com>
2024-07-16 23:09:01 +0200

    adding more tests

--- spec/schema.spec.mjs
+++ spec/schema.spec.mjs
@@ -4,6 +4,9 @@ describe(('Schemas'), () => {
         it('should', async () => {
             const schemaLoader = new SchemaLoader('./spec/schemas/complex.schema.json');
             const models = await getModels(schemaLoader);
+            const { instance } = models.find(x => x.type.name === 'AddressModel');
+            const id = instance.Id;
+            const postalCode = instance.postalCode;
             expect(models.length).toBe(2);
         });
     });
