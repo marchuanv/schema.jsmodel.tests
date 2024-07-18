82e675c448e89282244a9bf5b35c16278c8586e9
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-07-18 22:59:37 +0200

    updated specs

--- spec/schema.spec.mjs
+++ spec/schema.spec.mjs
@@ -1,15 +1,10 @@
-import { getModels, SchemaLoader } from 'schema.jsmodel';
+import { getModels } from 'schema.jsmodel';
 describe(('Schemas'), () => {
     describe(('given a schema'), () => {
-        it('should', async () => {
-            const schemaLoader = new SchemaLoader('./spec/schemas/complex.schema.json');
-            const models = await getModels(schemaLoader);
-            const { instance } = models.find(x => x.type.name === 'AddressModel');
-            instance.postalCode = 1232134;
-            instance.Id = 'awdawdawd';
-            const id = instance.Id;
-            const postalCode = instance.postalCode;
-            expect(models.length).toBe(2);
+        it('should export models', async () => {
+            const { AddressModel, ComplexObjectModel } = await getModels('./spec/schemas/complex.schema.json');
+            expect(AddressModel).toBeDefined();
+            expect(ComplexObjectModel).toBeDefined();
         });
     });
 });
